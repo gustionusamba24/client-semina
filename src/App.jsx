c0be6d51a6959cef3513735d23aeed364563e2c9
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-11 14:56:06 +0700

    Add folders that needed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/categories">Categories</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
