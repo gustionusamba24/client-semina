4c8f364b2bf104056d958e782bc044e9f0667ed7
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-13 12:46:10 +0700

    Add routing in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,76 @@
 import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function Categories() {
+  return (
+    <>
+      <h1>Categories</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Categories</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              <Link to="/categories/24242424242424">Seminar</Link>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>
+              <Link to="/categories/42087409879820">Musik</Link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+function CategoriesDetail() {
+  let { id } = useParams();
+  return <h1>Categories : {id}</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <ul>
         <li>
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/categories">Categories</a>
+          <Link to="/categories">Categories</Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link to="/about">About</Link>
         </li>
       </ul>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="categories/:id" element={<CategoriesDetail />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
