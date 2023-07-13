fd9eec87d149418ded07dcc3bae12a985cf5c4a8
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-13 20:25:20 +0700

    Create sign in page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,13 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useParams,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
-
-function useQuery() {
-  const { search } = useLocation();
-
-  return React.useMemo(() => new URLSearchParams(search), [search]);
-}
-
-function Home() {
-  return <h1>Home</h1>;
-}
-
-function Categories() {
-  const query = useQuery();
-
-  console.log(query.get("page"));
-
-  return (
-    <>
-      <h1>Categories</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Categories</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>
-              <Link to="/categories/24242424242424">Seminar</Link>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>
-              <Link to="/categories/42087409879820">Musik</Link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  );
-}
-
-function CategoriesDetail() {
-  let { id } = useParams();
-  return <h1>Categories : {id}</h1>;
-}
-
-function About() {
-  return <h1>About</h1>;
-}
-
-function Login() {
-  const navigate = useNavigate();
-
-  return (
-    <>
-      <h1>Login</h1>
-      <button onClick={() => navigate("/")}>Submit</button>
-    </>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageSignin from "./pages/signin";
 
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/categories">Categories</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="categories" element={<Categories />} />
-        <Route path="categories/:id" element={<CategoriesDetail />} />
-        <Route path="about" element={<About />} />
+        <Route path="/" element={<>Home</>} />
+        <Route path="signin" element={<PageSignin />} />
       </Routes>
     </BrowserRouter>
   );
