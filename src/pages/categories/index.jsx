3505097fb3236991e8ab1f1172bbc884a37aacd1
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-17 16:47:19 +0700

    Consume API get categories

--- src/pages/categories/index.jsx
+++ src/pages/categories/index.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Table } from "react-bootstrap";
 import { Navigate } from "react-router";
 import SButton from "../../components/Button";
 import SBreadCrumb from "../../components/Breadcrumb";
 import SNavbar from "../../components/Navbar";
+import { config } from "../../configs";
 
 function CategoriesPage() {
   const token = localStorage.getItem("token");
+
+  const [data, setData] = useState([]);
+
+  console.log("data");
+  console.log(data);
+
+  useEffect(() => {
+    console.log("useEffect");
+    const getCategoriesAPI = async () => {
+      try {
+        const res = await fetch(
+          `${config.api_host_dev}/cms/categories`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(res.data);
+
+        setData(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCategoriesAPI();
+  }, []);
+
   if (!token) return <Navigate to="/signin" replace={true} />;
 
   return (
     <>
+      {console.log("render")}
       <SNavbar />
 
       <Container className="mt-3">
